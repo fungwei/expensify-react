32eed25e791be938fe3f9a4aeded1cc729d101db
Fung Wei Tan Tai <fungwei@goget.my>
2018-07-21 14:43:54 +0800

    r-12-07-array-destructuring-v2

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+//Object Destructuring
+
 // const person = {
 //   age: 29,
 //   location: {
@@ -14,14 +16,29 @@
 //   console.log(`It's ${temperatue} in ${city}`);
 // }
 
-const book = {
-  title: 'World War Z',
-  author: 'Max Brooks',
-  publisher: {
-    name: 'penguin'
-  }
-};
+// const book = {
+//   title: 'World War Z',
+//   author: 'Max Brooks',
+//   publisher: {
+//     name: 'penguin'
+//   }
+// };
 
-const {name: publisherName = 'Self Published'} = book.publisher
+// const {name: publisherName = 'Self Published'} = book.publisher
 
-console.log(publisherName);
+// console.log(publisherName);
+
+
+//
+//Array Destucturing
+//
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvanie', '19147'];
+
+const [, city, state = 'New York'] = address;
+
+console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [itemName, ,mediumPrice] = item;
+console.log(`A medium ${itemName} cost ${mediumPrice}`)
