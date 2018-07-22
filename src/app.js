dec89ecc4104eac3977d14c99ac223566bfb93d3
Fung Wei Tan Tai <fungwei@goget.my>
2018-07-22 17:35:04 +0800

    r-13-04-react-redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configueStore';
 import {addExpense, removeExpense, editExpense} from './actions/expenses';
@@ -14,11 +15,20 @@ const store = configureStore();
 store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: 1000}));
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 100, createdAt: 1000}));
 store.dispatch(setTextFilter('bill'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('water'));
+}, 3000);
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses);
 
 
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
 
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
