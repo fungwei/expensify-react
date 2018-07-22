c3f7a217f6904255b695941f9ea1153cdfb1fbe9
Fung Wei Tan Tai <fungwei@goget.my>
2018-07-22 18:18:33 +0800

    r-13-06-controlled-inputs

--- src/componenets/ExpenseListItem.js
+++ src/componenets/ExpenseListItem.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-export default ({ description, amount, createdAt}) => (
+
+const ExpenseListItem = ({ id, description, amount, createdAt, dispatch}) => (
   <div>
     <h3>Description: {description}</h3>
     <p>Amount: {amount}</p>
     <p>Created At: {createdAt}</p>
+    <button 
+      onClick={() => {
+        dispatch(removeExpense({id}))
+      }}
+    >Remove</button>
   </div>
 );
+
+export default connect()(ExpenseListItem);
