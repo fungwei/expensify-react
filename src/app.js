c56484cd4fe4faa14180f9f03a59ec2c0e3b0a1d
Fung Wei Tan Tai <fungwei@goget.my>
2018-07-30 18:23:48 +0800

    r-15-06-production-webpack

--- src/app.js
+++ src/app.js
@@ -12,15 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 const store = configureStore();
 
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 200, createdAt: 3000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 300, createdAt: 2000}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses);
-
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
